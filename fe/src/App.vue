00e202d53a3e43a975253db818372caa1266d000
saktialfansyahp <saktialfansyahp@gmail.com>
2023-11-04 17:58:51 +0700

    change logout action

--- fe/src/App.vue
+++ fe/src/App.vue
@@ -202,11 +202,10 @@
                 </MenuItem>
                 <MenuItem v-slot="{ active }">
                   <a
-                    href="#"
                     @click="handleLogout"
                     :class="[
                       active ? 'bg-gray-100' : '',
-                      'block px-4 py-2 text-sm text-gray-700',
+                      'block px-4 py-2 text-sm text-gray-700 cursor-pointer',
                     ]"
                     >Sign out</a
                   >
